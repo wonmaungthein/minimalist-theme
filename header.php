8d886b2263a0a2242e295f5414642fbf0bfa7bcd
Won Maung Thein <wonmaungthein@gmail.com>
2018-10-28 06:24:58 +0000

    Adding a link to logo to navigate to a different page

--- header.php
+++ header.php
@@ -24,16 +24,12 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-text.png"  alt="Glasgow Bike Store logo" />
+</a>
 			<?php
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+			
 			$minimalist_theme_description = get_bloginfo( 'description', 'display' );
 			if ( $minimalist_theme_description || is_customize_preview() ) :
 				?>
