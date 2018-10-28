47dd48848d353f88334affc54c4623f46675f090
Won Maung Thein <wonmaungthein@gmail.com>
2018-10-28 06:23:35 +0000

    Added logo in footer

--- footer.php
+++ footer.php
@@ -13,7 +13,8 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Glasgow Bike Store logo" />
+	<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'minimalist-theme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
